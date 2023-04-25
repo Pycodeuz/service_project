5eb0ea154ee7302e2a80724175f29ccb87a021cd
xolmomin <xolmomin@gmail.com>
2023-04-25 12:53:41 +0500

    add docker compose

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM python:3.11.0-alpine
+
+ENV PYTHONUNBUFFERED=1
+ENV PYTHONDONTWRITEBYTECODE=1
+
+WORKDIR /app
+COPY . /app
+
+RUN --mount=type=cache,id=custom-pip,target=/root/.cache/pip pip install -r /app/requirements.txt
+
+
+# docker build -t django_image .
+# docker build -t django_image -f Dockerfile .
+# docker run -p 8001:8000 --name django_container -d django_image
+# docker exec -it django_container python3 manage.py makemigrations
+# docker exec -it django_container python3 manage.py migrate
+
+# docker rmi django_image
+# docker image rm django_image
+# docker ps
+# docker container ls
+
+# docker run --name p8_db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=1 -d -p 5432:5432 postgres:alpine
+# docker exec -it -u postgres p8_db psql
+
+
+# docker-compose -f docker-compose.yml build
+# docker-compose build .
+# docker-compose build --no-cache
+# docker-compose up
+# docker-compose down -v
+# docker-compose down
+
+# dockerfile, docker-compose,docker-swarm
--- apps/models.py
+++ apps/models.py
@@ -1,81 +1,10 @@
-from django.db.models import Model, CharField, ForeignKey, CASCADE, IntegerField, DateTimeField, ImageField, \
-    BooleanField, PositiveSmallIntegerField, SET_NULL, TextField, TextChoices
+from django.db import models
+import uuid
 
 
-class Category(Model):
-    name = CharField(max_length=100)
-
-    class Meta:
-        verbose_name_plural = "Categories"
-
-    def __str__(self):
-        return self.name
+class Event(models.Model):
+    _id = models.UUIDField('id', primary_key=True, default=uuid.uuid4, editable=False)
+    details = models.TextField()
+    years_ago = models.PositiveIntegerField()
 
 
-from django.contrib import admin
-
-
-class Origin(Model):
-    name = CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-
-class Entity(Model):
-    class GENDER(TextChoices):
-        GENDER_MALE = 'male', "Male"
-        GENDER_FEMALE = 'female', "Female"
-        GENDER_OTHERS = 'unknown', "Others/Unknown"
-
-    name = CharField(max_length=100)
-    alternative_name = CharField(max_length=100, null=True, blank=True)
-
-    category = ForeignKey('apps.Category', CASCADE)
-    origin = ForeignKey('apps.Origin', CASCADE)
-    gender = CharField(max_length=100, choices=GENDER.choices)
-    description = TextField()
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        abstract = True
-
-
-class Tag(Model):
-    name = CharField(max_length=255)
-
-
-class Product(Model):
-    name = CharField(max_length=255)
-    image = ImageField(upload_to='products/%Y/%m/%d', null=True)
-    price = IntegerField(default=0, blank=True)
-    description = CharField(max_length=255)
-    tags = ForeignKey('apps.Tag', CASCADE, null=True)
-    category = ForeignKey('apps.Category', CASCADE)
-    created_at = DateTimeField(auto_now_add=True)
-
-
-class Hero(Entity):
-    is_immortal = BooleanField(default=True)
-
-    benevolence_factor = PositiveSmallIntegerField(help_text="How benevolent this hero is?")
-    arbitrariness_factor = PositiveSmallIntegerField(help_text="How arbitrary this hero is?")
-    # relationships
-    father = ForeignKey('self', SET_NULL, "+", null=True, blank=True)
-    mother = ForeignKey('self', SET_NULL, "+", null=True, blank=True)
-    spouse = ForeignKey('self', SET_NULL, "+", null=True, blank=True)
-
-    class Meta:
-        verbose_name_plural = "Heroes"
-
-
-class Villain(Entity):
-    is_immortal = BooleanField(default=False)
-
-    malevolence_factor = PositiveSmallIntegerField(help_text="How malevolent this villain is?")
-    power_factor = PositiveSmallIntegerField(help_text="How powerful this villain is?")
-    is_unique = BooleanField(default=True)
-    count = PositiveSmallIntegerField(default=1)
